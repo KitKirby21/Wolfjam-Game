816ba8ae879feb2e2b23e3cf18f4ea0d7fea88d6
Aldric Kirby Saligo <43484785+KitKirby21@users.noreply.github.com>
2023-11-03 22:05:49 -0400

    Set up player detection in enemy
    Set up player detection in enemy

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,20 +1,4 @@
 extends CharacterBody2D
 
-var speed = 200
-var motion = Vector2.ZERO
-var player = null
+@onready var ai = $AI
 
-func _physics_process(delta):
-	if player:
-		motion = position.direction_to(player.position) * speed
-	velocity = motion;
-	move_and_slide()
-
-
-
-func _on_area_2d_body_entered(body):
-	player = body
-
-
-func _on_area_2d_body_exited(body):
-	player = null
