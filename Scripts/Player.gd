51b6836e9fccacfffa969f8e638ec69062a887f6
Peter Catalano <pcatalano64@gmail.com>
2023-11-03 21:30:54 -0400

    kromer added

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,11 +8,15 @@ extends CharacterBody2D
 
 @export var damage = 1
 
+@export var kromer = 0
+
 @export var can_move = true
 
 @export var Bullet: PackedScene
 
 @onready var anim_player = $Sprite2D/AnimationPlayer
+
+@onready var gun_end = $GunEnd
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -28,6 +32,7 @@ func _get_input():
 func shoot():
 	var bullet_instance = Bullet.instance()
 	add_child(bullet_instance)
+	bullet_instance.global_position =
 
 func _physics_process(delta):
 	_get_input()
