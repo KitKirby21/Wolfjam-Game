edd59484444b5c8bfae963bd1a6cc8ebcb0053f8
Fives47 <92649317+Fives47@users.noreply.github.com>
2023-11-04 04:51:52 -0400

    Kind of functioal build? lets go

--- Scripts/BulletManager.gd
+++ Scripts/BulletManager.gd
@@ -5,4 +5,5 @@ func bullet_spawned(bullet, position, direction):
 	add_child(bullet)
 	bullet.global_position = position
 	bullet.set_direction(direction)
+	bullet.z_index = 3
 	bullet.global_rotation = direction.angle()
