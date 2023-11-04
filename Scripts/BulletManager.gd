7784c24c20df2c6523194c7c3df25b895405a722
Peter Catalano <pcatalano64@gmail.com>
2023-11-03 23:56:14 -0400

    Revert "Merge branch 'main' of https://github.com/KitKirby21/Wolfjam-Game"
    This reverts commit ce8ff1dd18110ea908080a37788d556cb2aae691, reversing
    changes made to 2c03a065d4b99aaae006c8d351152f71655e22fc.

--- Scripts/BulletManager.gd
+++ Scripts/BulletManager.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
-@export var bullets = []
 
 func bullet_spawned(bullet, position, direction):
 	add_child(bullet)
 	bullet.global_position = position
 	bullet.set_direction(direction)
 	bullet.global_rotation = direction.angle()
-	bullets.push_back(bullet)
