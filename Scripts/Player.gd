382993be650bcd5e3eb9865592557c9f3d052927
Billy Guzik <guzikbilly@gmail.com>
2023-11-04 13:59:01 -0400

    player (and cat) animations done

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,23 +13,23 @@ signal shot(bullet, position, direction)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-func _get_input():
-	var input_direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
-	velocity = input_direction * DungeonManager.speed
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 	if Input.is_action_just_pressed("swing"):
 		#print(self.position)
-		anim_player.play("Swing")
+		anim_player.play("swing")
+
+func _get_input():
+	var input_direction = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	velocity = input_direction * DungeonManager.speed
 
 func shoot():
 	var bullet_instance = Bullet.instantiate()
 	var target = get_global_mouse_position()
 	var direction = gun_end.global_position.direction_to(target).normalized()
 	BulletManager.bullet_spawned(bullet_instance, gun_end.global_position, direction, true)
+	anim_player.play("shoot")
 
 func _physics_process(delta):
 	_get_input()
@@ -38,6 +38,7 @@ func _physics_process(delta):
 
 
 func take_damage(dmg):
+	anim_player.play("take_damage")
 	print(dmg)
 
 
