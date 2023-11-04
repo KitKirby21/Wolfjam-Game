d5ef5de755c58a92b99f6c5faa48704d28209ea9
Fives47 <92649317+Fives47@users.noreply.github.com>
2023-11-04 13:16:28 -0400

    Dungeon Manager can read enemies now

--- Scripts/DungeonManager.gd
+++ Scripts/DungeonManager.gd
@@ -8,19 +8,23 @@ var health = 49
 var haste = 60
 var damage = 1
 var kromer = 0
-
+var Enemies;
 var pickup_instance;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var Enemies = get_tree().get_nodes_in_group("Enemies")
+	
 	var player = get_tree().get_first_node_in_group("player")
-
+	Enemies = get_tree().get_nodes_in_group("enemy")
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	return
+	Enemies = get_tree().get_nodes_in_group("enemy")
+	print(Enemies.size())
+	if(Enemies.size()<=2):
+		get_tree().change_scene_to_file("res://Scenes/Cafe.tscn")
 	
 func _chanceDrop(position):
 	pickup_instance = pickup.instantiate()
