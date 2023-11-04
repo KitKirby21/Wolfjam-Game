14eaec2907d68d99caffacfdc6b378fe2c60880e
Aldric Kirby Saligo <43484785+KitKirby21@users.noreply.github.com>
2023-11-04 00:18:48 -0400

    Refixed enemy
    Redid all enemy work

--- Scripts/BulletManager.gd
+++ Scripts/BulletManager.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func bullet_spawned(bullet, position, direction):
 	add_child(bullet)
+	bullet.add_to_group("bullet")
 	bullet.global_position = position
 	bullet.set_direction(direction)
 	bullet.global_rotation = direction.angle()
--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,4 +1,53 @@
 extends CharacterBody2D
 
-@onready var ai = $AI
+@export var SPEED = 100
 
+var health = 5
+
+enum State {
+	PATROL,
+	ENGAGE
+}
+
+@onready var player_detection_zone = $PlayerDetectionZone
+
+var current_state:int = State.PATROL:
+	get:
+		return current_state
+	set(new_state):
+		if new_state == current_state:
+			return
+		current_state = new_state
+var player = null
+
+func _physics_process(delta):
+	match current_state:
+		State.PATROL:
+			pass
+		State.ENGAGE:
+			var direction = (player.global_position - global_position).normalized()
+			var desired_velocity = direction * SPEED
+			var steering = (desired_velocity - velocity) * delta * 2.5
+			velocity += steering
+			move_and_slide()
+
+
+func _on_player_detection_zone_body_entered(body):
+	if body.is_in_group("player"):
+		print("Entered")
+		current_state = State.ENGAGE
+		player = body
+
+
+func _on_player_detection_zone_body_exited(body):
+	if body.is_in_group("player"):
+		print("Exited")
+		current_state = State.PATROL
+		player = null
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("bullet"):
+		print("Damaged")
+		health -= body.bullet_damage
+	pass # Replace with function body.
