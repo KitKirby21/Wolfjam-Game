7d4d09912172211fc5b93185b9261892361e41f8
Aldric Kirby Saligo <43484785+KitKirby21@users.noreply.github.com>
2023-11-04 03:16:43 -0400

    Moved Wander Code to Enemy
    Moved wander code to parent enemy script

--- Scripts/MeleeEnemy.gd
+++ Scripts/MeleeEnemy.gd
@@ -1,18 +1,5 @@
 extends "res://Scripts/Enemy.gd"
 
-@export var marker_group_name : String
-var positions : Array
-var temp_positions : Array
-var current_position : Marker2D
-
-func _ready():
-	add_to_group("enemy")
-	DungeonManager._chanceDrop(self.position)
-	
-	positions = get_tree().get_nodes_in_group(marker_group_name)
-	get_positions()
-	get_next_position()
-
 func _physics_process(delta):
 	match current_state:
 		State.PATROL:
@@ -22,12 +9,3 @@ func _physics_process(delta):
 			
 		State.ENGAGE:
 			move(player.global_position, delta)
-
-func get_positions():
-	temp_positions = positions.duplicate()
-	temp_positions.shuffle()
-
-func get_next_position():
-	if temp_positions.is_empty():
-		get_positions()
-	current_position = temp_positions.pop_front()
